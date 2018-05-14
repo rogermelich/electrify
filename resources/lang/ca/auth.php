5433ce1812ca5bf8b161cee47aa2c4b0d0ca99ba
Roge Melich <rogermelich@gmail.com>
2018-05-14 18:36:28 +0200

    Translate

---
+++ resources/lang/ca/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Aquestes credencials no coincideixen amb els nostres registres.',
+    'throttle' => 'Hi ha massa intents d\'inici de sessió. Torna-ho a provar: segons segons.',
+
+];
